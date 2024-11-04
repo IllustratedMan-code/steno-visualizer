11e93dc31ca6b7ec84d580d31a30c3c5deaf2478
IllustratedMan-code <davidalewis00@gmail.com>
2024-11-04 16:59:44 +0000

    stable

--- src/chapter_1.md
+++ src/chapter_1.md
@@ -1,3 +1,13 @@
 # Chapter 1
 
-<steno-outline stroke="H-L"></steno-outline>
+## With Animations!
+
+<steno-outline stroke="H-L/WORLD"><steno-outline>
+
+## Without Animations!
+
+<steno-outline stroke="H-L/WORLD" noAnimate><steno-outline>
+
+## Change the width!
+
+<steno-outline stroke="H-L/WORLD" width="50%"><steno-outline>
--- steno-viz.js
+++ steno-viz.js
@@ -3,7 +3,7 @@ function getThemeColors() {
 
   // Fetch specific color variables used by mdBook
   const backgroundColor = rootStyle.getPropertyValue("--fg").trim();
-  const textColor = rootStyle.getPropertyValue("--fg").trim();
+  const textColor = rootStyle.getPropertyValue("--bg").trim();
 
   return { backgroundColor, textColor };
 }
@@ -12,17 +12,21 @@ class SVGHelper {
   constructor(svg) {
     // Just create a document fragment to parse the SVG
     const parser = new DOMParser();
-    const doc = parser.parseFromString(svg, "text/html");
+    const doc = parser.parseFromString(svg, "image/svg+xml");
     this.shadowRoot = doc;
     this.shadowRoot.querySelectorAll(".pressed > path").forEach((element) => {
-      element.style.stroke = getThemeColors().textColor;
+      element.style.stroke = getThemeColors().backgroundColor;
     });
     this.shadowRoot
       .querySelectorAll(".notpressed > path ")
       .forEach((element) => {
-        element.style.stroke = getThemeColors().textColor;
-        element.style.fill = getThemeColors().textColor;
+        element.style.stroke = getThemeColors().backgroundColor;
+        element.style.fill = getThemeColors().backgroundColor;
       });
+    this.shadowRoot.querySelectorAll(".notpressed > g ").forEach((element) => {
+      element.style.stroke = getThemeColors().textColor;
+      element.style.fill = getThemeColors().textColor;
+    });
   }
   get string() {
     const serializer = new XMLSerializer();
@@ -35,14 +39,37 @@ class StenoViz extends HTMLElement {
     /^(?<num>#)?(?:(?<ls>S)?(?<lt>T)?(?<lk>K)?(?<lp>P)?(?<lw>W)?(?<lh>H)?(?<lr>R)?)?(?:(?<la>A)?(?<lo>O)?)?(?<star>\*)?(?:(?<re>E)?(?<ru>U)?(?:-)?)?(?:(?<rf>F)?(?<rr>R)?(?<rp>P)?(?<rb>B)?(?<rl>L)?(?<rg>G)?(?<rt>T)?(?<rs>S)?(?<rd>D)?(?<rz>Z)?)?$/;
   static steno_outline_svg_cache = null;
 
-  static observedAttributes = ["stroke", "steno_outline", "opacity"];
+  static observedAttributes = [
+    "stroke",
+    "steno_outline",
+    "opacity",
+    "noAnimate",
+    "delay",
+    "width",
+  ];
 
   get stroke() {
-    return this.getAttribute("stroke");
+    return this.getAttribute("stroke") || "";
+  }
+  get strokes() {
+    return this.stroke.split("/");
   }
 
   get opacity() {
     return this.getAttribute("opacity") || "0.0";
+  }
+
+  get animate() {
+    return !this.hasAttribute("noAnimate");
+  }
+
+  get delay() {
+    return Number(this.getAttribute("delay")) || 1000;
+  }
+
+  animation_frame() {
+    this._animationframe = (this._animationframe + 1) % this.strokes.length;
+    return this._animationframe;
   }
 
   async steno_outline() {
@@ -61,21 +88,34 @@ class StenoViz extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (
-          mutation.type === "attributes" &&
-          mutation.attributeName === "class"
-        ) {
-          this.connectedCallback();
-        }
-      });
+    this.observer = new MutationObserver(() => {
+      this.connectedCallback();
     });
+    this._timestamp = 0;
+    this._animation_frame = 0;
+    this.render_loop = this.render_loop.bind(this);
   }
 
   async connectedCallback() {
-    console.log(getThemeColors());
-    this.shadowRoot.innerHTML = await this.render(this.stroke);
+    // Start observing the HTML element for class changes
+    this.observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+    if (this.animate && this.strokes.length > 0) {
+      requestAnimationFrame(this.render_loop);
+    } else {
+      html = "";
+      for (const i of this.strokes) {
+        html = html + (await this.render(i));
+      }
+      this.shadowRoot.innerHTML = html;
+    }
+  }
+
+  disconnectedCallback() {
+    // Clean up the observer when the element is removed
+    this.observer.disconnect();
   }
 
   async attributeChangedCallback(name, oldValue, newValue) {
@@ -85,6 +125,14 @@ class StenoViz extends HTMLElement {
   async render(stroke) {
     // Load base SVG
     const svg = new SVGHelper(await this.steno_outline());
+    if (this.hasAttribute("width")) {
+      console.log(svg.shadowRoot.documentElement);
+      svg.shadowRoot.documentElement.setAttribute(
+        "width",
+        this.getAttribute("width")
+      );
+      svg.shadowRoot.documentElement.removeAttribute("height");
+    }
     // Create helper with pressed SVG
 
     // Get the matched keys
@@ -102,6 +150,16 @@ class StenoViz extends HTMLElement {
       });
     }
     return svg.string;
+  }
+  async render_loop(timestamp) {
+    const elapsed = timestamp - this._timestamp;
+    if (elapsed >= this.delay) {
+      let frame = this._animation_frame;
+      this.shadowRoot.innerHTML = await this.render(this.strokes[frame]);
+      this._timestamp = timestamp;
+      this._animation_frame = (this._animation_frame + 1) % this.strokes.length;
+    }
+    requestAnimationFrame(this.render_loop);
   }
 }
 
